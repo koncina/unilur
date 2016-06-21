f8550563df8b9e55d48d7ff13a97451a671673ae
Eric Koncina <mail@koncina.eu>
2016-06-21 09:53:45 +0200

    change merge.list when one argument is NULL

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,7 @@
 # Merges two lists without creating duplicate elements
-# Used to merge arguments such as includes or css (HTML)
+# Used to merge arguments such as includes
 merge.list <- function(l1, l2) {
-  if (length(l1) == 0) return(l2)
-  if (length(l2) == 0) return(l1)
+  if (length(l1) * length(l2) == 0) return(c(l1, l2))
   keys <- unique(c(names(l1), names(l2)))
   Map(c, setNames(l1[keys], keys), setNames(l2[keys], keys))
 }
