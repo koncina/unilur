450553129d30bd2c6bb85262e0a681a98f772008
Eric Koncina <mail@koncina.eu>
2018-08-30 12:27:16 +0200

    import merge_list and rename merge.list to merge_includes

--- R/unilur_base.R
+++ R/unilur_base.R
@@ -45,9 +45,6 @@ unilur_base <- function(format, is_solution_format, suffix) {
   format$post_processor <- function(metadata, input_file, output_file, clean, verbose) {
     if (is.function(orig_processor)) orig_processor(metadata, input_file, output_file, clean, verbose)
     new_name = paste0(tools::file_path_sans_ext(output_file), suffix, ".", tools::file_ext(output_file))
-    print("aaaaaaaaaaaaa")
-    print(output_file)
-    print(new_name)
     file.rename(output_file, new_name)
     new_name
   }
@@ -55,7 +52,7 @@ unilur_base <- function(format, is_solution_format, suffix) {
   format$pre_knit <-  function(input, ...) {
     knitr::opts_hooks$set(solution = function(options) {
       # Using merge_list: opts.label will not work here
-      options <- knitr:::merge_list(knitr::opts_template$get("solution"), options)
+      options <- merge_list(knitr::opts_template$get("solution"), options)
       # No need to evaluate solutions if we don't show them...
       # TODO: might cause troubles on dependencies: not showing the solution but using it even for questions...
       if (!is_solution_format) {
--- R/utils.R
+++ R/utils.R
@@ -2,10 +2,12 @@
 
 # Merges two lists without creating duplicate elements
 # Used to merge arguments such as includes
-merge.list <- function(l1, l2) {
+merge_includes <- function(l1, l2) {
   if (length(l1) * length(l2) == 0) return(c(l1, l2))
   keys <- unique(c(names(l1), names(l2)))
   Map(c, setNames(l1[keys], keys), setNames(l2[keys], keys))
 }
+
+merge_list <- knitr:::merge_list
 
 `%n%` <- knitr:::`%n%`
