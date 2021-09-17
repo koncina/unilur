d2beb0440e01f22491eac1f48ca7db4757d80905
Aurelien Ginolhac <ginolhac@gmail.com>
2021-09-17 11:32:23 +0200

    answer_rmd header date in ISO8601 format

--- R/answer_rmd.R
+++ R/answer_rmd.R
@@ -1,15 +1,15 @@
 #' Convert to a tutorial answer Rmarkdown file
 #'
 #' Format to generate a Rmd file with solutions chunks being replaced by empty answer chunks.
-#' 
+#'
 #' @param yaml a list to override the \code{title}, \code{author} and \code{output} of the answer Rmd file.
-#' 
+#'
 #' @param suffix Suffix which is added to the filename (default is '_answer')
-#' 
+#'
 #' @param exclude_chunk keyword to use for excluding specific chunks in answer templates (default NULL)
-#' 
+#'
 #' @return R Markdown output format to pass to \code{\link[rmarkdown]{render}}
-#' 
+#'
 #' @export
 answer_rmd <- function(yaml = NULL, suffix = "_answer", exclude_chunk = NULL) {
   # Here we implement a fake knitting: I don't think it's possible to avoid using pandoc
@@ -20,14 +20,15 @@ answer_rmd <- function(yaml = NULL, suffix = "_answer", exclude_chunk = NULL) {
   e$default_yaml <- list(
     title = "My answers",
     author = "My name",
+    date = format(Sys.time(), "%Y-%m-%d"),
     output = "html_document"
   )
   yaml <- check.options(yaml, name.opt = "default_yaml", envir = e)
-  
+
   format <- rmarkdown::md_document()
-  
+
   answer_output <- NULL
-  
+
   format$pre_knit <-  function(input, ...) {
     # Creating answer Rmd
     rmd <- paste(readLines(input), collapse = "\n")
@@ -40,7 +41,7 @@ answer_rmd <- function(yaml = NULL, suffix = "_answer", exclude_chunk = NULL) {
     rmd <- gsub(pattern, replacement, rmd, perl = TRUE)
     if (!is.null(exclude_chunk)) {
       # Removing chunk with specific keyword user provided
-      pattern <- paste0(" *``` *{.*(?i)", 
+      pattern <- paste0(" *``` *{.*(?i)",
                         as.character(exclude_chunk),
                         "(?-i).*} *\\n[\\s\\S]*?``` *")
       rmd <- gsub(pattern, "", rmd, perl = TRUE)
@@ -51,21 +52,21 @@ answer_rmd <- function(yaml = NULL, suffix = "_answer", exclude_chunk = NULL) {
     # Tab character seems not accepted by the custom yaml parser... Try to use the rmarkdown parser?
     #header <- sprintf("---\ntitle: \"%s\"\nauthor: \"%s\"\ndate: '`r format(Sys.time(), \"%%d %%B, %%Y\")`'\noutput: %s\n---",
     #                  yaml[["title"]], yaml[["author"]], yaml[["output"]])
-    
-    header <- do.call(sprintf, c(fmt = "---\ntitle: \"%s\"\nauthor: \"%s\"\ndate: '`r format(Sys.time(), \"%%d %%B, %%Y\")`'\noutput: %s\n---",
-                                 yaml[c("title", "author", "output")]))
-    
+
+    header <- do.call(sprintf, c(fmt = "---\ntitle: \"%s\"\nauthor: \"%s\"\ndate: \"%s\"\noutput: %s\n---",
+                                 yaml[c("title", "author", "date", "output")]))
+
     rmd <- gsub(pattern, header, rmd, perl = TRUE)
-    
+
     answer_output <<- paste0(tools::file_path_sans_ext(input), suffix, ".Rmd")
     writeLines(rmd, answer_output)
-    
+
     knitr::opts_hooks$set(eval = function(options) {
       options$eval <- FALSE
       options
     })
   }
-  
+
   format$post_processor <- function(metadata, input_file, output_file, clean, verbose) {
     # Removing knitted output_file we don't want and using the already created answer_output as the generated output_file
     unlink(output_file)
